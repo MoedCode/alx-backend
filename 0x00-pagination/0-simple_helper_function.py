294a4873e25574000a24178b0fedf42cc9be46aa
MoedCode <mohamed71291@gmail.com>
2024-05-05 03:11:09 +0300

    0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-def index_range(page , page_size):
+def index_range(page, page_size):
     """ Calculate the start and end indices for pagination."""
     end = page_size * page
     start = end - page_size
     return (start, end)
+
 
 if __name__ == "__main__":
     res = index_range(1, 7)
@@ -14,4 +15,3 @@ if __name__ == "__main__":
     res = index_range(page=3, page_size=15)
     print(type(res))
     print(res)
-
