bae9bed8e27c2ae45982491bf2e9de2916536a10
MoedCode <mohamed71291@gmail.com>
2024-05-07 00:39:47 +0300

    2:5

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -60,7 +60,14 @@ class Server:
         prev_page = page - 1 if page - 1 >= 2 else None
 
         Idx = index_range(page, page_size)
-        return {'page_size': page_size, 'page': page, 'data': data, 'next_page': next_page, 'prev_page': prev_page, 'total_pages': total_pages}
+        return {
+            'page_size': page_size,
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
 
 
 def index_range(page, page_size):
