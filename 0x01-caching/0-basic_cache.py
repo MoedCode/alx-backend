9408c7596839f7c782a87d151c63c2c6827ade96
MoedCode <mohamed71291@gmail.com>
2024-05-09 00:20:29 +0300

    T0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,16 +5,16 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """BasicCache class inherits from BaseCaching and represents a caching system with no limit.  """
+    """BasicCache class inherits from BaseCaching and represents """
 
     def put(self, key, item):
-        """Add an item to the cache if both key and item are not None."""
+        """Add an item to the cache if both key and item are """
         if key is not None and item is not None:
             self.cache_data[key] = item
         return
 
     def get(self, key):
-        """Retrieve an item from the cache if key exists and its value is not None."""
+        """Retrieve an item from the cache if key exists and ."""
         if key in self.cache_data and key is not None:
             return self.cache_data[key]
         return None
