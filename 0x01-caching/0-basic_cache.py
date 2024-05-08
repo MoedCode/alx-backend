d7e922e068e9fdfd81d4f2f12dac3fdde2deaec3
MoedCode <mohamed71291@gmail.com>
2024-05-09 00:03:02 +0300

    T0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+""" BasicCache module
+"""
 from base_caching import BaseCaching
 
 
@@ -13,7 +15,7 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """Add an item to the cache if both key and item are not None."""
         if key is not None and item is not None:
-            self.cache_data[key] = item
+            return self.cache_data[key] = item
 
     def get(self, key):
         """Retrieve an item from the cache if key exists and its value is not None."""
