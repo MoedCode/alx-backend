fcd1c46f78b349c303e4ebdf672fee465cb7ca6b
MoedCode <mohamed71291@gmail.com>
2024-05-09 03:11:41 +0300

    T1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,1 +1,44 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" class FIFOCache  module
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    "class FIFOCache that inherits from BaseCaching and is a caching system:"
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key=None, item=None):
+        """Add an item to the cache if both key and item are """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) > self.MAX_ITEMS:
+            first_in = list(self.cache_data.keys())[0]
+            if key not in self.cache_data:
+                print("DISCARD: {}".format(first_in))
+                del self.cache_data[first_in]
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieve an item from the cache if key exists and ."""
+        if key in self.cache_data and key is not None:
+            return self.cache_data[key]
+        return None
+
+
+if __name__ == "__main__":
+
+    my_cache = FIFOCache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.put("D", "School")
+    my_cache.print_cache()
+    my_cache.put("E", "Battery")
+    my_cache.print_cache()
+    my_cache.put("C", "Street")
+    my_cache.print_cache()
+    my_cache.put("F", "Mission")
+    my_cache.print_cache()
--- 0x01-caching/temp.py
+++ 0x01-caching/temp.py
@@ -3,7 +3,35 @@
 # import base_caching # => 1
 from base_caching import *  # => 2
 
-# x =BaseCaching() # => 0
-# x =base_caching. BaseCaching() # => 1
-x = BaseCaching()   # => 2
-print(x.__dict__)
+
+class PlateStack:
+    def __init__(self, capacity=4):
+        self.stack = []
+        self.capacity = capacity
+
+    def add_plate(self, plate):
+        if len(self.stack) >= self.capacity:
+            # If the stack is full, remove the first plate (FIFO)
+            removed_plate = self.stack.pop(0)
+            print(f"Plate removed: {removed_plate}")
+        self.stack.append(plate)
+        print(f"Plate added: {plate} stack now {self.stack}")
+
+    def print_stack(self):
+        print("Current stack:")
+        print(self.stack)
+
+
+# Create a PlateStack instance
+plate_stack = PlateStack()
+
+# Add plates to the stack
+plate_stack.add_plate("Plate 1")
+plate_stack.add_plate("Plate 2")
+plate_stack.add_plate("Plate 3")
+plate_stack.add_plate("Plate 4")
+plate_stack.print_stack()
+
+# Try to add another plate
+plate_stack.add_plate("Plate 5")
+plate_stack.print_stack()
