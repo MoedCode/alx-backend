9c1bea72afb00043f200fd24a939d411c3ea5101
MoedCode <mohamed71291@gmail.com>
2024-05-15 02:38:18 +0300

    0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,10 +8,12 @@ from flask_babel import Babel
 app = Flask(__name__)
 bable = Babel(app)
 
-@app.route('/')
+
+@app.route('/',  strict_slashes=False)
 def index():
     """ Define route for the home page"""
-    return render_template("index.html")
+    return render_template("0-index.html")
+
 
 if __name__ == "__main__":
     app.run(port=5000, host='0.0.0.0', debug=True)
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+"""First you will setup a basic bable Flask app """
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+bable = Babel(app)
+
+
+@app.route('/',  strict_slashes=False)
+def index():
+    """ Define route for the home page"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0', debug=True)
